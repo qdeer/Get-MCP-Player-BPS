c6601d6b042a114fb1b3528e15379d237c0ddcef
qdeer <38561814+qdeer@users.noreply.github.com>
2022-09-16 18:04:56 +0100

    Update getspeed.java

--- getspeed.java
+++ getspeed.java
@@ -2,4 +2,4 @@ Minecraft mc = Minecraft.getMinecraft();
 
 Vec3 vec = new Vec3(mc.thePlayer.lastTickPosX, mc.thePlayer.lastTickPosY, mc.thePlayer.lastTickPosZ);
 DecimalFormat decFormat = new DecimalFormat();
-Dawn.addChatMessage(String.valueOf(decFormat.format(mc.thePlayer.getPositionVector().distanceTo(vec) * 20)));
+mc.thePlayer.sendChatMessage(String.valueOf(decFormat.format(mc.thePlayer.getPositionVector().distanceTo(vec) * 20)));
