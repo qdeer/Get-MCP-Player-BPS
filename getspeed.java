811fbc1b329e992426bd89e3f37f8f087e1ceb36
qdeer <38561814+qdeer@users.noreply.github.com>
2022-11-17 10:27:08 +0000

    Update getspeed.java
    java

--- getspeed.java
+++ getspeed.java
@@ -1,1 +1,4 @@
-print("Hello World")
+Minecraft mc = Minecraft.getMinecraft();
+
+Vec3 vec = new Vec3(mc.thePlayer.lastTickPosX, mc.thePlayer.lastTickPosY, mc.thePlayer.lastTickPosZ);
+double bps = mc.thePlayer.getPositionVector().distanceTo(vec) * 20;
